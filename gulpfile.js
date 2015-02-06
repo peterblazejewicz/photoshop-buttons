e1ccef897aa7881844e118fe9c2952175dbe444e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-02-06 23:30:42 +0100

    Add LESS build with live reload - now one can run SASS build - or choose LESS build both supported via BS 3.* via Bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-ruby-sass');
+var less = require('gulp-less');
 //
 gulp.task('browser-sync', function() {
   browserSync({
@@ -26,7 +27,22 @@ gulp.task('sass', function () {
     .pipe(browserSync.reload({stream: true}));
 });
 //
-gulp.task('default', ['sass', 'browser-sync'], function() {
+gulp.task('less', function() {
+  return gulp.src('less/style.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest('stylesheets'))
+    .pipe(browserSync.reload({stream: true}));
+});
+//
+gulp.task('default', ['dev']);
+//
+gulp.task('dev', ['sass', 'browser-sync'], function() {
   gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('*.html', ['browser-sync-reload']);
+});
+//
+gulp.task('dev:less', ['less', 'browser-sync'], function() {
+  gulp.watch('less/**/*.less', ['less']);
   gulp.watch('*.html', ['browser-sync-reload']);
 });
